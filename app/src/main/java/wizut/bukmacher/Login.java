64d15f6a3a16ad18be62209b2eec20d3f7d69ed9
Wiktor Dudzicz <wdudzicz@wi.zut.edu.pl>
2017-04-27 16:55:29 +0200

    Zmiana języka

--- app/src/main/java/wizut/bukmacher/Login.java
+++ app/src/main/java/wizut/bukmacher/Login.java
@@ -8,6 +8,6 @@ public class Login extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_wdlogin);
+        setContentView(R.layout.activity_login);
     }
 }
--- app/src/main/java/wizut/bukmacher/MainActivity.java
+++ app/src/main/java/wizut/bukmacher/MainActivity.java
@@ -1,9 +1,6 @@
 package wizut.bukmacher;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -12,25 +9,41 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    ListView lv_main;
+    List arrayMain;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        //TODO: Dodać przycisk refresh
+
+        arrayMain = new ArrayList<>();
+        arrayMain.add("Football");
+        arrayMain.add("Volleyball");
+        arrayMain.add("Handball");
+        arrayMain.add("Basketball");
+        arrayMain.add("Tennis");
+        arrayMain.add("Hockey");
+        arrayMain.add("Baseball");
+        arrayMain.add("Winter sports");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, R.layout.list_element, R.id.lv_element, arrayMain);
+
+        lv_main = (ListView) findViewById(R.id.lv_main);
+        lv_main.setAdapter(arrayAdapter);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -80,17 +93,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_bets) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_my_bets) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_my_account) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_settings) {
 
         }
 
