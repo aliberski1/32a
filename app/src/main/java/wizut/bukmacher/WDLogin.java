11611ec13095bd9752029c1e1af66eda1ed56212
Wiktor Dudzicz <wdudzicz@wi.zut.edu.pl>
2017-04-27 13:20:40 +0200

    Layouty logowania, pulpitu oraz menu nawigacyjnego

---
+++ app/src/main/java/wizut/bukmacher/WDLogin.java
@@ -0,0 +1,13 @@
+package wizut.bukmacher;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WDLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wdlogin);
+    }
+}
